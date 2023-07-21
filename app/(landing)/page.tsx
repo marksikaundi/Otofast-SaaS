cf04c593679700cd76cb1aae09c42422b6c26a9d
marksikaundi <emmanuelmark117@gmail.com>
2023-07-21 23:37:58 +0200

    landing page auth

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage = () => {
-  return <div>am landing (Unprotected)</div>;
+  return (
+    <div>
+      am landing (Unprotected)
+      <div>
+        <Link href="/sign-in">
+          <Button>Login</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
