4ff69f213b9e13b47198caf7b8fe738e7edd98d1
marksikaundi <emmanuelmark117@gmail.com>
2023-07-21 21:42:36 +0200

    created dashboard

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+const DashboadPage = () => {
+  return (
+    <Button variant="destructive" >Click me</Button>
+  )
+}
+
+export default DashboadPage
