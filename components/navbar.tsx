3a79a35610cfbb7957ddf6ab08baf953a0766a92
marksikaundi <emmanuelmark117@gmail.com>
2023-07-22 00:32:41 +0200

    DASH

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import { Menu } from "lucide-react";
 
 const Navbar = () => {
@@ -7,6 +8,9 @@ const Navbar = () => {
       <Button variant="ghost" size="icon" className="md:hidden">
         <Menu />
       </Button>
+      <div className="flex w-full justify-end">
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </div>
   );
 };
