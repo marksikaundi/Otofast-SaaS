1235440d05b10cde09c25d4fabdc1b964228a036
marksikaundi <emmanuelmark117@gmail.com>
2023-07-21 19:56:26 +0200

    btn pack

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <p className="text-6xl text-green-500">Hello world, Otofast SaaS</p>
+    <Button variant="destructive" >Click me</Button>
   )
 }
