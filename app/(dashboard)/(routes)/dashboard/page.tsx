92a1c5e495892ddc6956a157e83e854fa1bcc207
marksikaundi <emmanuelmark117@gmail.com>
2023-07-22 00:27:12 +0200

    cln

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,8 @@
-import { UserButton } from "@clerk/nextjs"
 
 const DashboadPage = () => {
   return (
     <div>
       <p>Dashboard page (Protected)</p>
-      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
