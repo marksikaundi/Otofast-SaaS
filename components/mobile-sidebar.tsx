11425135f2a9071a88bfc93e94ab5ac92f14d922
marksikaundi <emmanuelmark117@gmail.com>
2023-07-22 11:19:30 +0200

    bugs fix

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,18 +1,25 @@
 "use client";
-
+import { useState, useEffect } from "react";
 import { Button } from "./ui/button";
 import { Menu } from "lucide-react";
 import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-  } from "@/components/ui/sheet"
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
-  
+
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <Sheet>
       <SheetTrigger>
@@ -21,7 +28,7 @@ const MobileSidebar = () => {
         </Button>
       </SheetTrigger>
       <SheetContent side="left" className="P-0">
-<Sidebar/>
+        <Sidebar />
       </SheetContent>
     </Sheet>
   );
