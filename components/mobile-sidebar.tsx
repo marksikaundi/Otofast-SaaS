7ef02ef6faddb353a629f770e3d783840ef1c2e1
marksikaundi <emmanuelmark117@gmail.com>
2023-07-22 09:38:52 +0200

    side-component

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -2,8 +2,16 @@
 
 import { Button } from "./ui/button";
 import { Menu } from "lucide-react";
-import { Sheet, SheetTrigger } from "@/components/ui/sheet";
-
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+import Sidebar from "@/components/sidebar";
+  
 const MobileSidebar = () => {
   return (
     <Sheet>
@@ -12,6 +20,9 @@ const MobileSidebar = () => {
           <Menu />
         </Button>
       </SheetTrigger>
+      <SheetContent side="left" className="P-0">
+<Sidebar/>
+      </SheetContent>
     </Sheet>
   );
 };
