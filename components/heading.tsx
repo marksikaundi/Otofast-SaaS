26a67764b5641806c11e8419c9df8466d429dcf7
marksikaundi <emmanuelmark117@gmail.com>
2023-07-22 12:53:31 +0200

    added the heading

---
+++ components/heading.tsx
@@ -0,0 +1,19 @@
+import { LucideIcon } from "lucide-react";
+
+interface HeadingProps {
+  title: string;
+  description: string;
+  icon: LucideIcon;
+  iconColor?: string;
+  bgColor?: string;
+}
+
+export const Heading = ({
+  title,
+  description,
+  icon,
+  iconColor,
+  bgColor,
+}: HeadingProps) => {
+  return <div>Heading components</div>;
+};
