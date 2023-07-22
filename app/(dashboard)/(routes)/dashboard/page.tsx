0621639ac1d6c2f322d94d095724ad562db76613
marksikaundi <emmanuelmark117@gmail.com>
2023-07-22 11:47:27 +0200

    working on dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,11 @@
-
 const DashboadPage = () => {
   return (
     <div>
-      <p>Dashboard page (Protected)</p>
+      <div className="md-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">Explore the power of AI</h2>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DashboadPage
+export default DashboadPage;
