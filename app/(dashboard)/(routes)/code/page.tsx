989d3bffc4ad88c04ada834f1475bb807b60b134
marksikaundi <emmanuelmark117@gmail.com>
2023-07-23 18:11:02 +0200

    finished code component

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -18,6 +18,7 @@ import { Loader } from "@/components/loader";
 import { cn } from "@/lib/utils";
 import { UserAvatar } from "@/components/user-avatar";
 import { BotAvatar } from "@/components/bot-avatar";
+import ReactMarkdown from "react-markdown";
 
 const CodePage = () => {
   const router = useRouter();
@@ -127,7 +128,21 @@ const CodePage = () => {
                 )}
               >
                 {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
-                <p className="text-sm">{message.content}</p>
+                <ReactMarkdown 
+                components={{
+                  pre: ({ node, ...props}) => (
+                    <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg ">
+                        <pre {...props}/>
+                    </div>
+                  ),
+                  code: ({ node, ...props}) => (
+                    <code className="bg-black/10 rounded-lg p-1" {...props}/>
+                  )
+                }}
+                className="text-sm overflow-hidden leading-7"
+                >
+                  {message.content || ""}
+                  </ReactMarkdown>
               </div>
             ))}
           </div>
