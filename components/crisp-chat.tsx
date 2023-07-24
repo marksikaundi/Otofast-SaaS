11ab85f783aa2c56c1dce48d1479c2c713e93216
marksikaundi <emmanuelmark117@gmail.com>
2023-07-25 01:31:18 +0200

    rn

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure("8254f25a-eca0-421e-b4de-2bf655c03887");
+    Crisp.configure("8cbe020c-4ef2-4239-8520-6f742241120c");
   }, []);
 
   return null;
