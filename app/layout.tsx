d72a31a9921e4d959d3b8cea22ea07f485efd7a6
marksikaundi <emmanuelmark117@gmail.com>
2023-07-24 21:10:48 +0200

    header title changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import './globals.css'
 const font = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Genius',
+  title: 'Otofast Pro',
   description: 'AI Platform',
 }
 
